75c7be5422132bc577d67519ba442622fcdb3383
Fatih Baş <47625725+fatih-bas@users.noreply.github.com>
2022-04-20 23:26:30 +0300

    added eslint disable-next-line comment

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -22,6 +22,7 @@ const CardList = () => {
           completed: false,
       })));
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [cards]);
 
   const handleClick = (card) => {
